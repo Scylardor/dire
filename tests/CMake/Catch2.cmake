81c4122c661b7168286d5fe7317420a8733f6de1
Alexandre Baron <baron.alx@gmail.com>
2023-01-21 13:54:40 -0500

    + Integrating CTest with the unit test program

--- tests/CMake/Catch2.cmake
+++ tests/CMake/Catch2.cmake
@@ -1,11 +1,12 @@
-function(ADD_CATCH2_DEPENDENCY DOWNLOAD_PREFIX GIT_VERSION TARGET_NAME SCOPE WITH_MAIN CXX_STANDARD)
-	if(NOT TARGET Catch2::Catch2) # If Catch2 has not been found, try to download it from Github
+function(ADD_CATCH2_DEPENDENCY DOWNLOAD_PREFIX GIT_VERSION TARGET_NAME SCOPE CATCH_TARGET CXX_STANDARD)
+	if(NOT TARGET ${CATCH_TARGET}) # If Catch2 has not been found, try to download it from Github
 		include(FetchContent)
 		FetchContent_Declare(
 			Catch2
 			PREFIX ${DOWNLOAD_PREFIX}
 			GIT_REPOSITORY https://github.com/catchorg/Catch2.git
 			GIT_TAG        ${GIT_VERSION}
+			OVERRIDE_FIND_PACKAGE
 		)
 
 		# Kind of a hack to force FetchContent to comply with the C++ flags we want.
@@ -21,12 +22,7 @@ function(ADD_CATCH2_DEPENDENCY DOWNLOAD_PREFIX GIT_VERSION TARGET_NAME SCOPE WIT
 		set(CMAKE_CXX_STANDARD_REQUIRED "${CMAKE_CXX_STANDARD_REQUIRED_OLD}")
 	endif()
 
-	if(WITH_MAIN)
-		target_link_libraries(${TARGET_NAME} PRIVATE Catch2::Catch2WithMain)
-		add_dependencies(${TARGET_NAME} Catch2::Catch2WithMain)
-	else()
-		target_link_libraries(${TARGET_NAME} PRIVATE Catch2::Catch2)
-		add_dependencies(${TARGET_NAME} Catch2::Catch2)
-	endif()
+	target_link_libraries(${TARGET_NAME} PRIVATE ${CATCH_TARGET})
+	add_dependencies(${TARGET_NAME} ${CATCH_TARGET})
 
 endfunction()
